617789aeef27db85f5a8b3258284a06c34429f4c
Kosmalski <kamil.kosmalski@q-perior.com>
2020-09-16 13:06:54 +0200

    first draft of cross model saving in mongo db from soap connector

--- gensoapn/common/models/number-conv-model.js
+++ gensoapn/common/models/number-conv-model.js
@@ -1,18 +1,27 @@
 'use strict';
 
-module.exports = function(Numberconvmodel) {
+module.exports = function (Numberconvmodel) {
+    let app = require('../../server/server');
 
     Numberconvmodel.numberToWords = function (num, cb) {
-        Numberconvmodel.NumberToWords( {ubiNum: num}, function(err, response) {
+        Numberconvmodel.NumberToWords({ ubiNum: num }, function (err, response) {
             var result = response;
             cb(err, result);
+            const numStore = app.models.numStore;
+            
+            numStore.create({num:"testtwo"}, () => {
+                console.log("creation completed ")
+            })
+
+
+
         });
-        
+
     };
 
     Numberconvmodel.remoteMethod(
         'numberToWords', {
-        accepts: [            
+        accepts: [
             {
                 arg: 'num', type: 'number', required: true,
                 http: { source: 'query' }
